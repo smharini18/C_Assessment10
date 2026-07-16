d75f771dd305f6851fa54890ae1ff8dc0270b137
smharini18 <smharini18@gmail.com>
2026-07-16 15:57:06 +0530

    Create C_Program_2.c

---
+++ C_Program_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter the single number : ");
+    scanf(" %d",&num);
+    printf("The ASCII value of %d : %c",num,num);
+    return 0;
+}
